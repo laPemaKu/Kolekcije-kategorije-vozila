2c540038707f10140f58927ee9b39531d0fa2347
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-10-02 11:28:01 +0200

    Add files via upload

---
+++ Kolekcije-Kategorije_vozila_xml/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Kolekcije_Kategorije_vozila_xml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i = 2023; i > 1769; i--)
+            {
+                comGodine.Items.Add(i);
+            }
+        }
+        List<Vozilo> voziloList;
+        
+        private void btnUnos_Click(object sender, EventArgs e)
+        {
+            
+                string model = txtModel.Text;
+                int godina_proizvodnje = Convert.ToInt32(comGodine.SelectedItem.ToString()), broj_kotača = Convert.ToInt32(txtBrojKotaca.Text);
+                Vozilo vozilo = new Vozilo(model, godina_proizvodnje, broj_kotača);
+                voziloList.Add(vozilo);
+            
+        }
+
+        private void btnObrada_Click(object sender, EventArgs e)
+        {
+            /*foreach(Vozilo vozilo in voziloList)
+            {
+
+            }*/
+        }
+    }
+}
---
+++ Kolekcije-Kategorije_vozila_xml/Vozilo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcije_Kategorije_vozila_xml
+{
+    internal class Vozilo
+    {
+        private string model;
+        private int broj_kotaca;
+        private int godina_proizvodnje;
+
+        public string Model { get => model; set => model = value; }
+        public int Godina_proizvodnje { get => godina_proizvodnje; set => godina_proizvodnje = value; }
+        public int Broj_kotaca { get => broj_kotaca; set => broj_kotaca = value; }
+
+        public Vozilo(string model, int godina_proizvodnje, int broj_kotaca)
+        {
+            this.model = model;
+            this.godina_proizvodnje = godina_proizvodnje;
+            this.broj_kotaca = broj_kotaca;
+        }
+
+        public void OdabirKategorije()
+        {
+            if(this.broj_kotaca == 2)
+            {
+                Motocikl motocikl = new Motocikl(this.model, this.godina_proizvodnje, this.broj_kotaca);
+            }
+            if(this.broj_kotaca == 4)
+            {
+                Automobil automobil = new Automobil(this.model, this.godina_proizvodnje, this.broj_kotaca);
+            }
+            if (this.broj_kotaca > 4)
+            {
+                Kamion kamion = new Kamion(this.model, this.godina_proizvodnje, this.broj_kotaca);
+            }
+        }
+    }
+
+    class Motocikl : Vozilo
+    {
+        public Motocikl(string model, int godina_proizvodnje, int broj_kotaca) : base(model, godina_proizvodnje, broj_kotaca)
+        {
+
+        }
+    }
+    class Automobil : Vozilo
+    {
+        public Automobil(string model, int godina_proizvodnje, int broj_kotaca) : base(model, godina_proizvodnje, broj_kotaca)
+        {
+
+        }
+    }
+    class Kamion : Vozilo
+    {
+        public Kamion(string model, int godina_proizvodnje, int broj_kotaca) : base(model, godina_proizvodnje, broj_kotaca)
+        {
+
+        }
+    }
+}
